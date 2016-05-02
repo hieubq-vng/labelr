193ef79740d2962163f9dc9991961822b99b0e32
hieubq <hieubq@vng.com.vn>
2016-05-02 23:58:40 +0700

    Mon May  2 23:58:40 ICT 2016

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
+require('babel-core/register')
 
+var fs = require('fs')
 var webpack = require('webpack')
 var getConfig = require('hjs-webpack')
+var React = require('react')
+var ReactDOMServer = require('react-dom/server')
+var PublicPage = require('./src/pages/public').default
+var Layout = require('./src/layout').default
 
-module.exports = getConfig({
+var config = getConfig({
   in: 'src/app.js',
   out: 'public',
   clearBeforeBuild: true,
@@ -10,30 +16,41 @@ module.exports = getConfig({
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css']
-      },
-      {
-        test: /\.styl$/,
-        loaders: ['style', 'stylus']
-      },
-      {
-        test: /\.jsx$/,
-        loaders: ['babel']
-      }
-    ]
-  },
+  // module: {
+  //   loaders: [
+  //     {
+  //       test: /\.css$/,
+  //       loaders: ['style', 'css']
+  //     },
+  //     {
+  //       test: /\.styl$/,
+  //       loaders: ['style', 'stylus']
+  //     },
+  //     {
+  //       test: /\.jsx$/,
+  //       loaders: ['babel']
+  //     }
+  //   ]
+  // },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.optimize.UglifyJsPlugin()
   ],
   html: function (context) {
+    const publicPage = ReactDOMServer.renderToString(React.createElement(PublicPage))
+    const layoutPage = ReactDOMServer.renderToString(React.createElement(Layout, {me: {}}))
+
     return {
-      'index.html': context.defaultTemplate(),
-      '200.html': context.defaultTemplate()
+      'index.html': context.defaultTemplate({html: `<div id='root'>${publicPage}</div>`}),
+      '200.html': context.defaultTemplate({html: `<div id='root'>${layoutPage}</div>`})
     }
   }
 })
+
+// Having hmre present in the .babelrc will break with the `babel-core/register` above
+// so wait until that is done and then add it here via the loader query
+const babelrc = JSON.parse(fs.readFileSync('./.babelrc'))
+babelrc.env = {development: {presets: ['react-hmre']}}
+config.module.loaders[0].query = babelrc
+
+module.exports = config
