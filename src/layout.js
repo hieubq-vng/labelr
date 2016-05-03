ae00dd4001188271221904a2e2fd63486cb47ac3
hieubq <hieubq@vng.com.vn>
2016-05-03 10:46:51 +0700

    Tue May  3 10:46:51 ICT 2016

--- src/layout.js
+++ src/layout.js
@@ -2,10 +2,49 @@ import React from 'react'
 import ampersandMixin from 'ampersand-react-mixin'
 import NavHelper from './components/nav-helper'
 
-export default React.createClass({
-  mixins: [ampersandMixin],
+// export default React.createClass({
+//   mixins: [ampersandMixin],
 
-  displayName: 'Layout',
+//   displayName: 'Layout',
+
+//   render () {
+//     const {me} = this.props
+
+//     return (
+//       <NavHelper>
+//         <nav className='top-nav top-nav-light cf' role='navigation'>
+//           <input id='menu-toggle' className='menu-toggle' type='checkbox' />
+//           <label htmlFor='menu-toggle'>
+//             Menu
+//           </label>
+//           <ul className='list-unstyled list-inline cf'>
+//             <li>
+//               Labelr
+//             </li>
+//             <li>
+//               <a href='/repos'>Repos</a>
+//             </li>
+//             <li className='pull-right'>
+//               <span>{me.login} </span>
+//               <a href='/logout'>Logout</a>
+//             </li>
+//           </ul>
+//         </nav>
+//         <div className='container'>
+//           {this.props.children}
+//         </div>
+//       </NavHelper>
+//     )
+//   }
+// })
+
+class Layout extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.props.mixins = [ampersandMixin]
+    this.props.displayName = 'Layout'
+  }
 
   render () {
     const {me} = this.props
@@ -36,4 +75,6 @@ export default React.createClass({
       </NavHelper>
     )
   }
-})
+}
+
+export default Layout
